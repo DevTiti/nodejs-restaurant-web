0fa3cc2cb036674d91b5cc7f2239af471cc965e4
Petrit Kabashi <python.engineer10@gmail.com>
2024-08-29 00:22:13 +0200

    Final testing

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import "./Header.scss";
+import Logo from "../../assets/images/Zomato-Logo.png";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import blackLogo from "../../assets/images/blackLogo.webp";
+
+const Header = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="header">
+      <nav>
+        <span>Get the App</span>
+        <div className="right">
+          <span>Investor Relations</span>
+          <span>Add restaurant</span>
+          <span>Log in</span>
+          <span>Sign up</span>
+        </div>
+      </nav>
+      <div className="hamburger" onClick={() => setOpen(!open)}>
+        {open ? <CloseIcon style={{ color: "black" }} /> : <MenuIcon />}
+      </div>
+      {open && (
+        <div className="sideMenu">
+          <img src={blackLogo} alt="logo" />
+          <div className="innerMenu">
+            <span>Investor Relations</span>
+            <span>Add restaurants</span>
+            <span>Log In</span>
+            <span>Sign Up</span>
+          </div>
+        </div>
+      )}
+      <div className="headerContent">
+        <img src={Logo} alt="logo" />
+        <h3>Discover the best food & drinks in Istanbul</h3>
+        <div className="input">
+          <select name="" id="">
+            <option value="Etiler">Etiler</option>
+            <option value="Kadiköy">Kadiköy</option>
+            <option value="Eminönü">Eminönü</option>
+            <option value="Üsküdar">Üsküdar</option>
+            <option value="Karaköy">Karaköy</option>
+          </select>
+          |
+          <input
+            type="text"
+            placeholder="Search for restaurant, cuisine or a dish"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
